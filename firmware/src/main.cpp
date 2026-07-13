b8b23e11b04ab947d2a4ea33ef7fd05c6a3a190a
TimotheeDvt <timothee.dravet@gmail.com>
2026-07-13 11:14:48 +0200

    firmware: update ip

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -10,7 +10,7 @@ const char *WIFI_SSID = "MUSIC-TIM";
 const char *WIFI_PASSWORD = "T8329#n5";
 
 WiFiUDP Udp;
-const IPAddress outIp(10, 42, 0, 1);
+const IPAddress outIp(192, 168, 12, 1);
 const uint16_t outPort = 8000;
 const uint16_t localPort = 8888;
 
